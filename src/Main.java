b620417990dfaf148d98f75c2879d4dc370b9039
Minh Hieu <25021770@vnu.edu.vn>
2026-03-28 22:28:05 +0700

    Hihihi<3

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+import Model.User;
+
+public class Main {
+    public static void main(String[] args) {
+        User user = new User("Hieu", "12/04/2007");
+        System.out.println(user.getName());
+    }
+}
---
+++ src/Model/User.java
@@ -0,0 +1,14 @@
+package Model;
+
+public class User {
+    private String name, dob;
+
+    public User(String name, String dob) {
+        this.name = name;
+        this.dob = dob;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
