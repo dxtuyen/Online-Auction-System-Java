8b24d7cf2fdbb3b17d2f5626b0f1a306da0f9ff7
Xuan Tuyen <tuyendoxuan05@gmail.com>
2026-04-01 16:57:23 +0700

    hello

--- src/main/java/com/auction/model/Entity/Admin.java
+++
@@ -1,7 +0,0 @@
-package com.auction.model.Entity;
-
-public class Admin extends User {
-    public Admin(String username, String password, String email) {
-        super(username, password, email, Role.ADMIN);
-    }
-}
--- src/main/java/com/auction/model/Entity/Role.java
+++
@@ -1,7 +0,0 @@
-package com.auction.model.Entity;
-
-public enum Role {
-    BIDDER,
-    SELLER,
-    ADMIN
-}
