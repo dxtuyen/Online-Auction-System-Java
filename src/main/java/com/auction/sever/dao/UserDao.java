61ba7950b150bdc526e69e21dc0cc4fc15491c5a
Minh Hieu <25021770@vnu.edu.vn>
2026-04-11 19:15:31 +0700

    add userDao

---
+++ src/main/java/com/auction/sever/dao/UserDao.java
@@ -0,0 +1,24 @@
+package com.auction.sever.dao;
+
+import com.auction.model.entity.User;
+import java.util.List;
+
+public interface UserDao {
+    //Nhóm các hàm CRUD
+    int insertUser(User user); //Thêm một user mới
+
+    User getUserById(int id); //Lấy thoong tin user theo id
+
+    boolean updateUser(User user); //Cập nhật thông tin user
+
+    boolean deleteUser(int id); //Xóa user(Admin)
+
+    //Nhóm các hàm nghiệp vụ
+    User getUserByUsername(String username); //Tìm kiếm user
+
+    List<User> getAllUsers(); //Lấy danh sách các user(Admin)
+
+    boolean isUsernameExists(String username); //Kiểm tra xem username tồn tại không
+
+    boolean isEmailExists(String email); //Kiểm tra xem email này có đã tồn tại chuwa
+}
